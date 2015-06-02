bc70bbf9b0dbd31ffd8807eda702c0575cbb3969
Ashaar Riaz <ashaar.riaz@pcni.org>
2015-06-03 05:19:54 +1000

    oauth2 commit

---
+++ src/main/java/org/openhims/oauth2/service/impl/AuthoritiesServiceImpl.java
@@ -0,0 +1,17 @@
+package org.openhims.oauth2.service.impl;
+
+import org.openhims.oauth2.dao.impl.AuthoritiesDAO;
+import org.openhims.oauth2.domain.Authorities;
+import org.openhims.oauth2.service.AuthoritiesService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AuthoritiesServiceImpl implements AuthoritiesService 
+{
+	@Autowired
+	private AuthoritiesDAO authoritiesDAO;
+	
+	public void saveAuthorities(Authorities authorities)
+	{
+		this.authoritiesDAO.save(authorities);
+	}
+}
